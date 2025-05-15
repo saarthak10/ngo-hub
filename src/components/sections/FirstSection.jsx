46be4fff9ffe7dbdf674514e5ecbc0b6a6651287
saarthak10 <saarthaksharma87@gmail.com>
2025-05-15 19:18:46 +0530

    home screen started

---
+++ src/components/sections/FirstSection.jsx
@@ -0,0 +1,15 @@
+import { Grid, Typography } from '@mui/material'
+import React from 'react'
+
+const FirstSection = () => {
+  return (
+    // <Grid>
+        <Typography>
+            TOP NGO LISTING
+        </Typography>
+    // </Grid>
+
+)
+}
+
+export default FirstSection
